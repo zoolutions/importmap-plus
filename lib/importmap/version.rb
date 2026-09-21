9df4283d1757d5e12532e8a99ad1c20d21543eaf
mhenrixon <mikael@mhenrixon.com>
2026-09-21 13:23:40 +0200

    Prepare for 2.0.0

--- lib/importmap/version.rb
+++ lib/importmap/version.rb
@@ -1,6 +1,6 @@
 module Importmap
   # importmap-plus versions independently of the importmap-rails it forks.
   # UPSTREAM_VERSION is the importmap-rails release this tracks.
-  VERSION = "1.2.0"
+  VERSION = "2.0.0"
   UPSTREAM_VERSION = "2.2.3"
 end
